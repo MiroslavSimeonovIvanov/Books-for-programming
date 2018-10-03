3dd81e8578a987c6d98035158cb22c764314480f
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2018-10-03 13:37:39 +0300

    Create min-and-max-value-of-number-data-type.js

---
+++ Algorithms in C - Robert Sedgewick/Scripts/min-and-max-value-of-number-data-type.js
@@ -0,0 +1,4 @@
+var minValueOfNumberDataType = Number.MIN_VALUE;
+var maxValueOfNumberDataType = Number.MAX_VALUE;
+
+console.log("Min value = " + minValueOfNumberDataType + "\n" + "Max value = " + maxValueOfNumberDataType);
