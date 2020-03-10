5eb368f69cb007f0fdfa4fa44a8bf1e0707dd629
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2020-03-10 20:48:52 +0200

    Delete quoted-and-not-quoted-string.js

--- Introduction to Programming with C#/Scripts/quoted-and-not-quoted-string.js
+++
@@ -1,5 +0,0 @@
-var quotedText = "The \"use\" of quotations causes difficulties.";
-//var notQuotedText = "The "use" of quotations causes difficulties.";
-
-console.log(quotedText);
-//console.log(notQuotedText);
