0e69ef02d4c475729ae52916251f22b96b5370fb
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2020-03-10 20:49:35 +0200

    Delete members-of-sequence.js

--- Introduction to Programming with C#/Scripts/members-of-sequence.js
+++
@@ -1,9 +0,0 @@
-var sequenceOfNumbers = [];
-
-for (let evenNumbers = 2, oddNumbers = -3; evenNumbers <= 100;
-    evenNumbers = evenNumbers + 2, oddNumbers = oddNumbers - 2) {
-  sequenceOfNumbers.push(evenNumbers, oddNumbers);
-}
-
-var result = sequenceOfNumbers.join(", ");
-console.log(result);
