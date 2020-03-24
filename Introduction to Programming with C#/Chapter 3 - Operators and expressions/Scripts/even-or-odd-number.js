db90922a42ffc87ac434f96c1a2a5e0c025a4a69
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2020-03-24 16:27:29 +0200

    Create even-or-odd-number.js

---
+++ Introduction to Programming with C#/Chapter 3 - Operators and expressions/Scripts/even-or-odd-number.js
@@ -0,0 +1,8 @@
+var number = Number(window.prompt("Hello user! Please type a number", ""));
+
+if (number % 2 === 0) {
+    console.log("The number you have entered is even.");
+}
+else {
+    console.log("The number you have entered is odd.");
+}
