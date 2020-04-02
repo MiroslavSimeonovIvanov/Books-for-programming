4954b17e561c48a1e0de21178f9c871d36579701
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2020-04-02 17:08:14 +0300

    Create integers-and-their-sum.js

---
+++ Introduction to Programming with C#/Chapter 4 - Console input and output/Scripts/integers-and-their-sum.js
@@ -0,0 +1,8 @@
+var firstNumber = Number(window.prompt("User please type a number", ""));
+var secondNumber = Number(window.prompt("OK! Now please type a second " +
+  "number", ""));
+var thirdNumber = Number(window.prompt("I'm really sorry but I need one " +
+  "last third number", ""));
+var sum = firstNumber + secondNumber + thirdNumber;
+
+console.log("The sum of the numbers you have entered is - " + sum);
