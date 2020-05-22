5e586ee2d68abb965efb48515c3522f5570f9b9a
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2020-05-22 15:08:31 +0300

    Create allocate-array-of-integers.js

---
+++ Introduction to Programming with C#/Chapter 7 - Arrays/Scripts/allocate-array-of-integers.js
@@ -0,0 +1,6 @@
+var arrayOfIntegers = new Array(20);
+
+for (let index = 0; index < arrayOfIntegers.length; index++) {
+  arrayOfIntegers[index] = index * 5;
+  console.log(arrayOfIntegers[index]);
+}
