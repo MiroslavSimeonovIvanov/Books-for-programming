4038e344fc497668095fdf9175929a9e1d04ff1a
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2020-06-05 16:50:08 +0300

    Create convert-decimal-to-binary-number.js

---
+++ Introduction to Programming with C#/Chapter 8 - Numeral systems/Scripts/convert-decimal-to-binary-number.js
@@ -0,0 +1,5 @@
+var decimalNumber = Number(window.prompt("Please enter decimal number", ""));
+var binaryNumber = decimalNumber.toString(2);
+
+console.log(`${decimalNumber} (Dec)`);
+console.log(`${binaryNumber} (Bin)`);
