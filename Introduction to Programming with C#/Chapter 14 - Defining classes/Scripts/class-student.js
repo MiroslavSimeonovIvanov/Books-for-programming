f1d4f31cd53fdbfb7870e4f72964d50f4e5580e6
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2020-09-09 15:45:32 +0300

    Create class-student.js

---
+++ Introduction to Programming with C#/Chapter 14 - Defining classes/Scripts/class-student.js
@@ -0,0 +1,14 @@
+class Student {
+  constructor(firstName, secondName, lastName, course, specialty, university,
+    email, telephoneNumber) {
+    this.name = "Student";
+    this.firstName = firstName;
+    this.secondName = secondName;
+    this.lastName = lastName;
+    this.course = course;
+    this.specialty = specialty;
+    this.university = university;
+    this.email = email;
+    this.telephoneNumber = telephoneNumber;
+  }
+}
