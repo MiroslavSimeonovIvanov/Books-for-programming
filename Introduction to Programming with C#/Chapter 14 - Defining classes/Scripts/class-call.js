d9e0a6c1a47d63226b141117698c205763482fb2
Miroslav Ivanov <MiroslavSimeonovIvanov@users.noreply.github.com>
2020-09-09 15:45:55 +0300

    Add files via upload

---
+++ Introduction to Programming with C#/Chapter 14 - Defining classes/Scripts/class-call.js
@@ -0,0 +1,7 @@
+class Call {
+  constructor(date, timeStarted, duration) {
+    this.date = date;
+    this.timeStarted = timeStarted;
+    this.duration = duration;
+  }
+}
---
+++ Introduction to Programming with C#/Chapter 14 - Defining classes/Scripts/class-mobile-phone.js
@@ -0,0 +1,10 @@
+class MobilePhone {
+  constructor(model, manufacturer, price, owner, battery, display) {
+    this.model = model;
+    this.manufacturer = manufacturer;
+    this.price = price;
+    this.owner = owner;
+    this.battery = battery;
+    this.display = display;
+  }
+}
